8d09101ade9764e79e571508c5fcd768cbdb56c3
SophrosyneEunoia <kelvinmulyawan2000@gmail.com>
2022-05-28 02:18:01 +0700

    add : aritmethic operator

---
+++ basic/03-ope-aritmatika.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var var1 = 50
+	var var2 = 20
+
+	var add = var1 + var2
+	fmt.Println("Hasil Tambah :",add)
+
+	var sub = var1 - var2
+	fmt.Println("Hasil Kurang :",sub)
+
+	var mul = var1 * var2
+	fmt.Println("Hasil Kali :",mul)
+
+	var div = var1 / var2
+	fmt.Println("Hasil Bagi :",div)
+}
